73ba3e9d7f539105f0daab7640d3aab936d1732f
Marcos Rivera <mjrivera@knowmadmood.com>
2026-05-18 18:06:53 +0200

    style: media queries comentadas

--- src/App.vue
+++ src/App.vue
@@ -57,12 +57,12 @@ nav a:first-of-type {
   border: 0;
 }
 
-@media (min-width: 1024px) {
+/* @media (min-width: 1024px) {
   header {
     display: flex;
     place-items: center;
     padding-right: calc(var(--section-gap) / 2);
-  }
+  } 
 
   .logo {
     margin: 0 2rem 0 0;
@@ -82,5 +82,5 @@ nav a:first-of-type {
     padding: 1rem 0;
     margin-top: 1rem;
   }
-}
+}*/
 </style>
