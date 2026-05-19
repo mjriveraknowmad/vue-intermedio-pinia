d8b5d6e49cf8523d2c4bc18c1c36515dc3e91251
Marcos Rivera <mjrivera@knowmadmood.com>
2026-05-19 09:41:51 +0200

    feat: Principio de estructura de componentes de clientes y enrutado

---
+++ src/clients/pages/ClientList.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import ClientList from '@/clients/components/ClientList.vue';
+</script>
+
+<template>
+    <div>
+        <h1>Listado de clientes</h1>
+
+        <!-- Client List -->
+        <ClientList :clients="[]" />
+
+        <!-- PaginationNumbers -->
+
+        <!-- LoadingModal -->
+
+    </div>
+</template>
+
+<style scoped></style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,22 +1,34 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Counter1Page from '@/counter/pages/Counter1Page.vue';
-import CounterSetupPage from '@/counter/pages/CounterSetupPage.vue';
+import { createRouter, createWebHistory } from "vue-router";
+import Counter1Page from "@/counter/pages/Counter1Page.vue";
+import CounterSetupPage from "@/counter/pages/CounterSetupPage.vue";
+import ClientsLayout from "@/clients/layout/ClientsLayout.vue";
+import ClientList from "@/clients/pages/ClientList.vue";
+import ClientPage from "@/clients/pages/ClientPage.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'counter-1',
-      component: Counter1Page
+      path: "/",
+      name: "counter-1",
+      component: Counter1Page,
     },
     {
-      path: '/counter-2',
-      name: 'counter-2',
-      component: CounterSetupPage
+      path: "/counter-2",
+      name: "counter-2",
+      component: CounterSetupPage,
     },
-    
-  ]
-})
+    {
+      path: "/clients",
+      name: "clients",
+      component: ClientsLayout,
+      redirect: { name: "list" },
+      children: [
+        { path: "list", name: "list", component: ClientList },
+        { path: "/clients/:id", name: "client-id", component: ClientPage },
+      ],
+    },
+  ],
+});
 
-export default router
+export default router;
